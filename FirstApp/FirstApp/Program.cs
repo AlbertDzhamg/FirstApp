17ae3cfbd1a98d267f0dc5fb7b3fdde53a0a4b2e
AlbertDzhamg <dzhamgar11@mail.ru>
2022-03-20 23:19:38 +0300

    lr2"

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace FirstApp
 {
@@ -6,7 +10,24 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<helloappdbContext>();
+            var options = optionsBuilder.UseSqlServer(connectionString).Options;
+            using (helloappdbContext db = new helloappdbContext())
+            {
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
         }
     }
 }
